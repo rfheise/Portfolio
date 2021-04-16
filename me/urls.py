691224e722bce457e1cba9ff05cecad3f9315b2a
rfheise <43056909+rfheise@users.noreply.github.com>
2021-04-16 18:19:36 -0400

    home

--- me/urls.py
+++ me/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path("",views.index, name = "index"),
+    path("",views.server("index.html"), name = "index"),
+    path("construction", views.server("construction.html")),
+    path("homburger", views.server("homburger.html"), name = "homburger")
 ]
